f25e6faec13db67c9c79bbef17789e742dd718ec
AtharvaKeskar2002 <keskaratharva27@gmail.com>
2023-09-30 22:34:56 +0530

    groovy script

---
+++ code/pipeline.groovy
@@ -0,0 +1,37 @@
+pipeline {
+    agent 'lable1'
+    
+    stages {
+            stage('Pull') {
+                steps {
+                        git 'https://github.com/AtharvaKeskar2002/studentui.git'
+                }
+            }
+            
+            stage('Build') {
+                steps {
+                        sh """
+                            cd /home/ubuntu/workspace
+                            sudo apt-get install maven -y
+                            mvn clean 
+                            mvn package
+                        """
+                    }
+            }
+
+            stage('Deploy') {
+                steps {
+                    sh """
+                            apt update -y
+                            sudo apt-get install unzip
+                            wget https://dlcdn.apache.org/tomcat/tomcat-10/v10.1.13/bin/apache-tomcat-10.1.13.zip
+                            sudo mkdir /opt/tomcat
+                            sudo unzip apache-tomcat-10.1.13.zip -d /opt/tomcat
+                            sudo chmod -R +x /opt/tomcat/apache-tomcat-10.1.13
+                            cp /home/ubuntu/workspace/target/studentapp.war /opt/tomcat/apache-tomcat-10.1.13/webapps/
+                    """
+                }
+            }
+        }
+    }
+}
