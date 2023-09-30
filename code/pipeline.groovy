6e1914ac71b5310bf5cd3e83c61a3bf0bebdbab6
AtharvaKeskar2002 <keskaratharva27@gmail.com>
2023-09-30 23:43:02 +0530

    hello

--- code/pipeline.groovy
+++ code/pipeline.groovy
@@ -30,7 +30,7 @@ pipeline {
                             sudo apt-get install unzip
                             wget https://dlcdn.apache.org/tomcat/tomcat-10/v10.1.13/bin/apache-tomcat-10.1.13.zip
                             cd
-                            sudo unzip apache-tomcat-10.1.13.zip -d 
+                            sudo unzip apache-tomcat-10.1.13.zip 
                             sudo chmod -R +x apache-tomcat-10.1.13
                             mv /home/ubuntu/workspace/atharva/target/studentapp-2.2-SNAPSHOT.war /home/ubuntu/workspace/atharva/target/student.war
                             cp /home/ubuntu/workspace/atharva/target/student.war apache-tomcat-10.1.13/webapps/
