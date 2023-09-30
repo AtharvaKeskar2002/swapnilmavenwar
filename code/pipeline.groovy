a1a433907d26005211710ef43c4a2556bb53165c
AtharvaKeskar2002 <keskaratharva27@gmail.com>
2023-10-01 00:52:49 +0530

    hello

--- code/pipeline.groovy
+++ code/pipeline.groovy
@@ -33,6 +33,7 @@ pipeline {
                             cd /home/ubuntu/workspace/atharva/target
                             mv studentapp-2.2-SNAPSHOT student.war
                             cd ..
+                            chmod 777 /home/ubuntu/workspace/atharva/apache-tomcat-8.5.93/webapps
                             mv student.war /home/ubuntu/workspace/atharva/apache-tomcat-8.5.93/webapps/student.war
                             cd apache-tomcat-8.5.93/bin/
                             ./catalina.sh start
